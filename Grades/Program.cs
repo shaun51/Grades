49bf70ae32b3f30ddb0f851327e0fa1da9aefb9f
Shaun <shaun.nichols51@gmail.com>
2016-10-18 18:38:14 -0700

    Initial Gradebook

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -10,6 +10,12 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+
+            GradeBook book2 = new GradeBook();
+            book.AddGrade(75);
         }
     }
 }
